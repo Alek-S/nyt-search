373a478aaeeff64e02c196e7bec351af495a3b48
Alek Shnayder <alek.shnayder@gmail.com>
2017-06-20 21:04:13 -0500

    Mocha testing folder/file setup

---
+++ test/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true,
+        "mocha": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const server = require('../server');
+const chalk = require('chalk');
+
+//===HTML Routes===
+describe( chalk.yellow('HTML Routes:'), function () {
+	afterEach(function () {
+		server.close();
+	});
+
+	it('responds to GET / with 200', function(done) {
+		request(server)
+			.get('/')
+			.expect(200, done);
+	});
+});
+
+
+
+//===API Routes===
+describe(chalk.yellow('API Routes:'), function () {
+	afterEach(function () {
+		server.close();
+	});
+
+});
