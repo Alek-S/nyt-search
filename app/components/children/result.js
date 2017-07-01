f1bf9c2ab530e8acddc57abe443952b945c836e5
Alek Shnayder <alek.shnayder@gmail.com>
2017-07-01 17:19:00 -0500

    Save articles from front

--- app/components/children/result.js
+++ app/components/children/result.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Saved from './saved.js';
+import axios from 'axios';
 
 class Result extends React.Component{
 	constructor(){
@@ -9,16 +10,28 @@ class Result extends React.Component{
 	_getHeadlines(){
 		let articles = this.props.searched;
 		console.log(articles[0].headline.web_url);
-		
 		return articles.map( (article)=>{ 
 			return( 
 				<p>{article.headline.main} --  
 					<strong> Published: </strong>{article.pub_date.slice(0,10)} -- 
 					<a href={article.web_url}>Link</a> 
-					<button className='btn'>Save Article</button></p>
+					<button className='btn' onClick={ this._saveArticle.bind(article) }>Save Article</button></p>
 			); 
 		});
 	}//end of _getHeadlines
+
+	_saveArticle(){
+		event.preventDefault();
+
+		// post to saved
+		axios.post('/api/saved',{
+			title: this.headline.main,
+			date: this.pub_date,
+			url: this.web_url
+		}).then((response)=>{
+			console.log(response.data);
+		});
+	}
 
 
 	render(){
