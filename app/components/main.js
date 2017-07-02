76e1c18deb33233ac2169a1a6aac1e86ee60a4c4
Alek Shnayder <alek.shnayder@gmail.com>
2017-07-01 20:35:40 -0500

    Add logo, github link to header

--- app/components/main.js
+++ app/components/main.js
@@ -6,8 +6,10 @@ class Main extends React.Component{
 		return(
 			<div>
 				<header>
-					<h1>New York Times Article Scrubber</h1>
+					<h1> <img src="assets/images/logo.svg" alt="logo" height="40px"/> New York Times Article Search</h1>
 					<h2> Search for and annotate articles of interest.</h2>
+
+					<a href="https://github.com/Alek-S/Scrapeosaurus" id="github">GitHub <img src="assets/images/external-link.svg" alt="link" height="10px" /></a>
 				</header>
 
 				<Search />
