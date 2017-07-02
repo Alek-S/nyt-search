7920936b5adbd704cd30b3366ee6c194888d8a48
Alek Shnayder <alek.shnayder@gmail.com>
2017-07-01 20:38:23 -0500

    Update github project page link

--- app/components/main.js
+++ app/components/main.js
@@ -9,7 +9,7 @@ class Main extends React.Component{
 					<h1> <img src="assets/images/logo.svg" alt="logo" height="40px"/> New York Times Article Search</h1>
 					<h2> Search for and annotate articles of interest.</h2>
 
-					<a href="https://github.com/Alek-S/Scrapeosaurus" id="github">GitHub <img src="assets/images/external-link.svg" alt="link" height="10px" /></a>
+					<a href="https://github.com/Alek-S/nytreact" id="github">GitHub <img src="assets/images/external-link.svg" alt="link" height="10px" /></a>
 				</header>
 
 				<Search />
